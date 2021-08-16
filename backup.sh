b722d2bac106effea56be8e465f1e07e06df5517
Mário Monteiro <marioammonteiro@gmail.com>
2021-08-16 23:34:18 +0100

    neopush server and client working, before change project name and start to create a starter from it

---
+++ backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DT=$(date +%Y-%m-%d-%H-%M)
+DIR=.bak
+FILE="$DIR/$DT.tgz"
+FILE_EXCLUDE=exclude.tag
+mkdir $DIR -p
+touch .yarn/$FILE_EXCLUDE
+touch node_modules/$FILE_EXCLUDE
+touch packages/neo-push/client/node_modules/$FILE_EXCLUDE
+touch packages/neo-push/server/node_modules/$FILE_EXCLUDE
+# touch packages/migration/node_modules/$FILE_EXCLUDE
+
+touch .bak/$FILE_EXCLUDE
+
+tar -zcvf $FILE \
+	--exclude-tag-all=$FILE_EXCLUDE \
+	.
