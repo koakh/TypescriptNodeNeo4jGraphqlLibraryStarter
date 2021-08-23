1f83286e4e9a08ca8a867360c2f44aee23acc7e7
Mário Monteiro <marioammonteiro@gmail.com>
2021-08-23 23:18:17 +0100

    work on neo4j and caddy

--- packages/neo4j-gqllib-starter/server/src/app/neo4j.ts
+++ packages/neo4j-gqllib-starter/server/src/app/neo4j.ts
@@ -2,7 +2,11 @@ import * as neo4j from 'neo4j-driver';
 import * as config from './config';
 import { createDebugger } from './debugger';
 
-export const driver = neo4j.driver(config.NEO_URL, neo4j.auth.basic(config.NEO_USER, config.NEO_PASSWORD));
+export const driver = neo4j.driver(
+  config.NEO_URL,
+  neo4j.auth.basic(config.NEO_USER, config.NEO_PASSWORD),
+  { encrypted: 'ENCRYPTION_ON' }
+);
 const debug = createDebugger('Neo4j');
 
 export async function connect() {
