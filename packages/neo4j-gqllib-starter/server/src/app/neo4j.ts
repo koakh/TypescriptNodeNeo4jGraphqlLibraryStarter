5548939675703ea3c6618b8d70ef3a3eb3cc9186
Mario <mario@vmi381400.contaboserver.net>
2021-08-31 19:22:55 +0200

    changes in contabo

--- packages/neo4j-gqllib-starter/server/src/app/neo4j.ts
+++ packages/neo4j-gqllib-starter/server/src/app/neo4j.ts
@@ -5,7 +5,7 @@ import { createDebugger } from './debugger';
 export const driver = neo4j.driver(
   config.NEO_URL,
   neo4j.auth.basic(config.NEO_USER, config.NEO_PASSWORD),
-  { encrypted: 'ENCRYPTION_ON' }
+  // { encrypted: 'ENCRYPTION_ON' }
 );
 const debug = createDebugger('Neo4j');
 
